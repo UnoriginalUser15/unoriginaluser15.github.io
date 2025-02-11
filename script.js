d8ebe22a528b4096269b87cd0d4fedd9e70f03a5
UnoriginalUser15 <mrwillowmoody@gmail.com>
2025-02-11 23:15:31 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(function(){
+    document.getElementById("start-up").style.display = "none";
+}, 5500);
+
+// 5500 is the actual time that should be used in the code above
+// 0 is just for testing/dev purposes
