fc7305a2af65e46daacb866d3adac08a0403ab4e
UnoriginalUser15 <mrwillowmoody@gmail.com>
2025-02-11 23:49:11 +0000

    fixed issue where desktop was visible during start up

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ setTimeout(function(){
     document.getElementById("start-up").style.display = "none";
 }, 5500);
 
+setTimeout(function(){
+    document.getElementById("os").style.display = "block";
+}, 5500);
+
 // 5500 is the actual time that should be used in the code above
 // 0 is just for testing/dev purposes
