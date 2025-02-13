5e8908b99cd90be22caf914c04088980db1539f8
Willow Moody <mrwillowmoody@gmail.com>
2025-02-13 04:14:37 +0000

    visual refinements and addition of a moveable window

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 setTimeout(function(){
     document.getElementById("start-up").style.display = "none";
     document.getElementById("os").style.display = "block";
-}, 5500);
+}, 0);
 
 // 5500 is the actual time that should be used in the code above
 // 0 is just for testing/dev purposes
+
+// make the openable windows draggable
+// yes this is shamelessly stolen from w3schools
+// i'm goint to adapt it more in the morning (it's 4:13 am)
+dragElement(document.getElementById("mydiv"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the window from
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the window from anywhere inside the window
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
