8f2317af280ff1d2d6ea003d19c0c2245cf32bde
UnoriginalUser15 <mrwillowmoody@gmail.com>
2025-02-14 16:40:10 +0000

    added functionality for multiple windows at once

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setTimeout(function(){
     document.getElementById("start-up").style.display = "none";
     document.getElementById("os").style.display = "block";
-}, 5500);
+}, 0);
 
 // 5500 is the actual time that should be used in the code above
 // 0 is just for testing/dev purposes
@@ -12,13 +12,24 @@ setTimeout(function(){
 
 // update: i tried to adapt it to make it so the window can't be dragged into the void
 // the important word there is 'tried' (i'm going to end it)
-dragElement(document.getElementById("window"));
 
-function dragElement(elmnt) {
+// finish the system that controls the window order
+
+var window_order = [];
+const desktop = document.getElementById("window-container");
+
+for (const window of desktop.children){
+    dragElement(window);
+    window_order.push(window.getAttribute("id"))
+}
+
+console.log(window_order)
+
+function dragElement(elmnt, window_order) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
+    if (elmnt.querySelector(".windowheader")) {
         // if present, the header is where you move the window from
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        elmnt.querySelector(".windowheader").onmousedown = dragMouseDown;
     } else {
         // otherwise, move the window from anywhere inside the window
         elmnt.onmousedown = dragMouseDown;
@@ -46,18 +57,26 @@ function dragElement(elmnt) {
         // set the element's new position
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+        // makes the selected window be above all other windows
+        const id = elmnt.getAttribute("id")
+        document.getElementById(id).style.zIndex = "50";
     }
 
     function closeDragElement() {
         // stop moving when mouse button is released
         document.onmouseup = null;
         document.onmousemove = null;
+        
+        // moves the window back to 10 on the z-index
+        const id = elmnt.getAttribute("id")
+        var new_window_order = window_order.filter()
     }
 }
 
 // closes a window that has been opened
-function close_window() {
-    var x = document.getElementById("window");
+function windowOpenClose(id) {
+    var x = document.getElementById(id);
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
