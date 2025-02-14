b1cbf080a1677db67bfe1cea1aa9771c7305fa19
Willow Moody <mrwillowmoody@gmail.com>
2025-02-14 18:07:16 +0000

    changed 'setTimeout' value to 5500

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 setTimeout(function(){
     document.getElementById("start-up").style.display = "none";
     document.getElementById("os").style.display = "block";
-}, 0);
+}, 5500);
 
 // 5500 is the actual time that should be used in the code above
 // 0 is just for testing/dev purposes
